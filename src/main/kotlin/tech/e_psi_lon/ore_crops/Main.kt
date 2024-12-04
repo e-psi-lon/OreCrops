966c7d8e278a883b0d328df17691d2d51c358345
e-psi-lon <theg41g@gmail.com>
2024-12-04 19:19:17 +0100

    feat: Added a tick and a 1second function and a reloading message

--- src/main/kotlin/tech/e_psi_lon/ore_crops/Main.kt
+++ src/main/kotlin/tech/e_psi_lon/ore_crops/Main.kt
@@ -2,31 +2,37 @@ package tech.e_psi_lon.ore_crops
 
 import io.github.ayfri.kore.arguments.chatcomponents.textComponent
 import io.github.ayfri.kore.arguments.colors.Color
-import io.github.ayfri.kore.arguments.numbers.TimeNumber
-import io.github.ayfri.kore.arguments.numbers.TimeType
+import io.github.ayfri.kore.arguments.enums.Relation
+import io.github.ayfri.kore.arguments.scores.ScoreboardCriteria
+import io.github.ayfri.kore.arguments.scores.score
+import io.github.ayfri.kore.arguments.types.literals.allPlayers
 import io.github.ayfri.kore.arguments.types.literals.literal
-import io.github.ayfri.kore.commands.function
-import io.github.ayfri.kore.commands.schedule
+import io.github.ayfri.kore.arguments.types.resources.FunctionArgument
+import io.github.ayfri.kore.commands.execute.execute
 import io.github.ayfri.kore.commands.scoreboard.scoreboard
+import io.github.ayfri.kore.commands.tellraw
 import io.github.ayfri.kore.configuration
 import io.github.ayfri.kore.dataPack
-import io.github.ayfri.kore.functions.function
 import io.github.ayfri.kore.functions.load
+import io.github.ayfri.kore.functions.tick
 import io.github.ayfri.kore.pack.pack
+import java.io.File
 import kotlin.io.path.Path
 
 const val NAMESPACE = "ore_crops"
 val LORE = textComponent("Ore Crops", color = Color.BLUE) { italic = true }
+const val PATH = "out"
 
 fun main() {
-	val dataPack = dataPack("OreCrops") {
+	File(PATH).deleteRecursively()
+	val dataPack = dataPack("Ore Crops") {
 		pack {
 			format = 55
-			description = textComponent("Makes ores growable!\n") +
+			description = textComponent("Adds mineral seeds.\n") +
 					textComponent("Created by e_psi_lon ", color = Color.GOLD) +
 					textComponent("(based on the HackFight data pack)", color = Color.GRAY)
 		}
-		path = Path("out")
+		path = Path(PATH)
 
 		configuration {
 			prettyPrint = true
@@ -41,7 +47,37 @@ fun main() {
 					add(literal("#$name.loaded"), "load.status", 1)
 				}
 			}
-			function("1s", NAMESPACE)
+			scoreboard {
+				objectives {
+					add("ore_plants.data", ScoreboardCriteria.DUMMY)
+					add("ore_plants.second", ScoreboardCriteria.DUMMY)
+				}
+			}
+			tellraw(allPlayers { tag = "convention.debug" }, textComponent("[",  color = Color.WHITE) +
+					textComponent(name, color = Color.GOLD ) +
+					textComponent("] (re)loaded.", color = Color.WHITE)
+			)
+
+		}
+		tick("tick", NAMESPACE) {
+			scoreboard {
+				players {
+					add(literal("#tick"), "$NAMESPACE.second", 1)
+				}
+			}
+			execute {
+				ifCondition {
+					this.score(literal("#tick"), "$NAMESPACE.second", 20, Relation.GREATER_THAN_OR_EQUAL_TO)
+				}
+				run("second", NAMESPACE) {
+					scoreboard {
+						players {
+							reset(literal("#tick"), "$NAMESPACE.second")
+						}
+					}
+				}
+				run(FunctionArgument("second", NAMESPACE, "generated_scopes"))
+			}
 		}
 	}
 	dataPack.generate()
